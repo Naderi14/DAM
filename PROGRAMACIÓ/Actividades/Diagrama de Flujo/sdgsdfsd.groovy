cd7e15b9b2de4996ffcb74353b1049b8562093d7
Ditarex <Ditarex95@gmail.com>
2024-10-01 15:16:16 +0200

    cositas

--- PROGRAMACIÓ/Actividades/Diagrama de Flujo/sdgsdfsd.groovy
+++ PROGRAMACIÓ/Actividades/Diagrama de Flujo/sdgsdfsd.groovy
@@ -6,7 +6,7 @@ Algoritmo NumeroMayor
 	Leer numero2;
 	Escribir "Introduce el número 3";
 	Leer numero3;
-	Si numero1 > numero2 Y numero1 > numero2 entonces;
+	Si numero1 > numero2 Y numero1 > numero3 entonces;
 			Escribir numero1, "es el mayor";
 	Sino;
 		Si numero2 > numero3 entonces;
@@ -29,11 +29,12 @@ Algoritmo OperarNumeros
 	producto = numero1 * numero2;
 	Si numero2 == 0 entonces;
 		Escribir "No se puede realizar la división por 0";
+		division = 0;
 	Sino;
 		division = numero1 / numero2;
 	FinSi;
-
-	Escribir "Suma:",suma,"Resta:",resta,""
+	Escribir "Suma:",suma,"Resta:",resta,"Producto:",producto,"Division:",division;
+FinAlgoritmo
 
 
 
