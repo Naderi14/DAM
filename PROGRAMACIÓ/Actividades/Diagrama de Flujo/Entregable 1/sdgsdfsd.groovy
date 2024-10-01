eaa06172a9169bd57e29e26aa353f1d7430ed133
Juan Francisco <juanfrancisco.sanchez.castel@gmail.com>
2024-10-01 09:51:23 +0200

    hola

---
+++ PROGRAMACIÓ/Actividades/Diagrama de Flujo/Entregable 1/sdgsdfsd.groovy
@@ -0,0 +1,40 @@
+Algoritmo NumeroMayor
+	Definir numero1, numero2, numero3 tipo enteros;
+	Escribir "Introduce el número 1";
+	Leer numero1;
+	Escribir "Introduce el número 2";
+	Leer numero2;
+	Escribir "Introduce el número 3";
+	Leer numero3;
+	Si numero1 > numero2 Y numero1 > numero2 entonces;
+			Escribir numero1, "es el mayor";
+	Sino;
+		Si numero2 > numero3 entonces;
+			Escribir numero2, "es el mayor";
+		Sino;
+			Escribir numero3, "es el mayor";
+		FinSi;
+	FinSi;
+FinAlgoritmo
+
+Algoritmo OperarNumeros
+	Definir numero1, numero2, suma, resta, producto tipo entero;
+	Definir division tipo real;
+	Escribir "Introduce numero1";
+	Leer numero1;
+	Escribir "Introduce numero2";
+	Leer numero2;
+	suma = numero1 + numero2;
+	resta = numero1 - numero2;
+	producto = numero1 * numero2;
+	Si numero2 == 0 entonces;
+		Escribir "No se puede realizar la división por 0";
+	Sino;
+		division = numero1 / numero2;
+	FinSi;
+
+	Escribir "Suma:",suma,"Resta:",resta,""
+
+
+
+
