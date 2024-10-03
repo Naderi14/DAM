35249fa41262d8f75daca5e25292dcbbd73e499b
Juan Francisco <juanfrancisco.sanchez.castel@gmail.com>
2024-10-03 08:34:57 +0200

    He creado el proyecto ensenazaEntorno para hacer ahi todas las clases en una estructura de clases bien diferencial para poderse organizar desde el principio, siempre mantener esta estructura pls

---
+++ temporal/enseñanzaEntorno/src/enseñanzaEntorno/ClasePrincipal.java
@@ -0,0 +1,20 @@
+/*
+* Capecera.java 26 Sent 2024
+*
+*
+* ©Copyright 2024 Juan Francisco Sanchez Castelblanque <ditarex95@alumnes.ilerna.com>
+*
+* This is free software, licensed under the GNU General Public License v3.
+* See http://www.gnu.org/licenses/gpl.html for more information.
+*/
+package enseñanzaEntorno;
+import java.util.Scanner;
+public class ClasePrincipal 
+{
+	public static void main(String[] args) 
+	{
+		Ejercicios ejercicios = new Ejercicios();
+		
+		ejercicios.condicionales();
+	}
+}
