9863a3967733bcdad925598f77a7124a82378c8e
Ditarex <Ditarex95@gmail.com>
2025-01-21 21:50:00 +0100

    por ahora esto de programacion, me voy a sobar

--- PROGRAMACIÓN_M03_M05/PROGRAMACIÓ/Actividades/Java/RA2/Entregable 6 - Clases; Client/m03_17_modular_ac02_client/src/client/Client.java
+++ PROGRAMACIÓN_M03_M05/PROGRAMACIÓ/Actividades/Java/RA2/Entregable 6 - Clases; Client/m03_17_modular_ac02_client/src/client/Client.java
@@ -11,7 +11,7 @@ public class Client {
     private float debts = 0;
     private boolean active = true;
 
-    Client(String accountNum, String name, String surname, String nationality, String phone, String dni, byte age)
+    public Client(String accountNum, String name, String surname, String nationality, String phone, String dni, byte age)
     {
         this.accountNum = accountNum;
         this.name = name;
@@ -22,11 +22,137 @@ public class Client {
         this.age = age;
     }
 
-    Client(String accountNum, String name, String surname, String dni)
+    public Client(String accountNum, String name, String surname, String dni)
     {
         this.accountNum = accountNum;
         this.name = name;
         this.surname = surname;
         this.dni = dni;
+    }
+
+    public String getAccount()
+    {
+        return this.accountNum;
+    }
+
+    public void setAccount(String accountNum)
+    {
+        this.accountNum = accountNum;
+    }
+
+    public String getName()
+    {
+        return this.name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getSurname()
+    {
+        return this.surname;
+    }
+
+    public void setSurname(String surname)
+    {
+        this.surname = surname;
+    }
+
+    public String getNationality()
+    {
+        return this.nationality;
+    }
+
+    public void setNationality(String nationality)
+    {
+        this.nationality = nationality;
+    }
+
+    public String getPhone()
+    {
+        return this.phone;
+    }
+
+    public void setPhone(String phone)
+    {
+        this.phone = phone;
+    }
+
+    public String getDni()
+    {
+        return this.dni;
+    }
+
+    public void setDni(String dni)
+    {
+        this.dni = dni;
+    }
+
+    public byte getAge()
+    {
+        return this.age;
+    }
+
+    public void setAge(byte age)
+    {
+        this.age = age;
+    }
+
+    public float getDebts()
+    {
+        return this.debts;
+    }
+
+    public void setDebts(float debts)
+    {
+        this.debts = debts;
+    }
+
+    public boolean isActive()
+    {
+        return this.active;
+    }
+
+    public void setActive(boolean active)
+    {
+        this.active = active;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "";
+    }
+
+    public boolean cancelDebts()
+    {
+        return false;
+    }
+
+    public boolean cancelDebts2()
+    {
+        return false;
+    }
+
+    public boolean reduceDebts(float debts)
+    {
+        return false;
+    }
+
+    public boolean reduceDebts2(float debts)
+    {
+        return false;
+    }
+
+    public boolean increaseDebts(float debts)
+    {
+        return false;
+    }
+
+    public boolean increaseDebts2(float debts)
+    {
+        return false;
     }
 }
