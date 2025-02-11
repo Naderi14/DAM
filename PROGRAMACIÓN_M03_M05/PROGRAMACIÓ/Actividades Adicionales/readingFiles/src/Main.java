0d3762e74c207917da531f1323708440fd723cb3
Ditarex <Ditarex95@gmail.com>
2025-02-12 00:09:11 +0100

    muchas cosas a corregir aqui

--- PROGRAMACIÓN_M03_M05/PROGRAMACIÓ/Actividades Adicionales/readingFiles/src/Main.java
+++ PROGRAMACIÓN_M03_M05/PROGRAMACIÓ/Actividades Adicionales/readingFiles/src/Main.java
@@ -1,11 +1,9 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 
 public class Main {
     private static FileReader fr;
+    private static FileWriter fw;
     private static File file;
     private static char[] caracteres;
     private static ArrayList listaCaracteres = new ArrayList<>();
@@ -16,18 +14,13 @@ public class Main {
         {
             file = new File("resources/Documento1.txt");
             fr = new FileReader("resources/Documento1.txt");
-        }
-        catch (FileNotFoundException e)
-        {
-            throw new RuntimeException(e);
-        }
 
-        try
-        {
+            // PRIMERA PRUEBA DE READER
             caracteres = new char[20];
-
             for (int i = 0; i < 5; i++)
             {
+                // Usamos la funcion read con el modo de pasarle el inicio de la lectura y la cantidad de caracteres a leer
+                // la cantidad le decimos que sea la misma que la longitud de nuestro array
                 fr.read(caracteres, 0, caracteres.length);
 
                 for (char ch : caracteres)
@@ -44,13 +37,13 @@ public class Main {
                 System.out.print(ch);
             }
 
-            fr.close();
+            System.out.println("\n\nValor ASCII de la letra a la que esta apuntando el reader ahora: " + fr.read() + " = " + (char)fr.read());
 
-            /*System.out.println("Valor ASCII de la primera letra del texto: " + fr.read());
-
-            // Cogeremos toda la longitud del archivo para luego imprimirla
+            // Cogeremos toda la longitud del archivo para luego imprimirla mediante la instancia de File que nos da las propiedades
             caracteres = new char[(int) file.length()];
+            // Leemos los caracteres que quepan en este array, por eso le pasamos el array como parametro de la funcion read
             fr.read(caracteres);
+            // Función para cerrar la lectura del archivo actual del FileReader
             fr.close();
             for (char ch : caracteres)
             {
@@ -70,14 +63,24 @@ public class Main {
             for (int i = 0; i < listaCaracteres.size(); i++)
             {
                 System.out.print(listaCaracteres.get(i));
-            }*/
+            }
+
+            fw = new FileWriter("resources/Documento1.txt");
+            // Vamos a escribir, sobreescribiendo el contenido del archivo
+            fw.write("Esto será ya todo el nuevo texto");
+            fw.close();
+            fr = new FileReader("resources/Documento1.txt");
+            caracteres = new char[(int) file.length()];
+            fr.read(caracteres);
+
+            for (char caracter : caracteres)
+            {
+                System.out.print(ch);
+            }
         }
         catch (IOException e)
         {
             System.out.println(e.getMessage());
         }
-
-
-
     }
 }
---
+++ temporal/excepcionesExtra/src/exceptions/AsientosInsuficientesException.java
@@ -0,0 +1,8 @@
+package exceptions;
+
+public class AsientosInsuficientesException extends Exception {
+    public AsientosInsuficientesException(String message)
+    {
+        super(message);
+    }
+}
